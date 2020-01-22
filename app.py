c833c40a09b2c5a16a09b91251d3e3f3ec5199ea
fdesmet <print.fdesmet@gmail.com>
2020-01-22 16:04:03 +0100

    imported os to catch the error if the json file was empty

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import json
+import os
+import os.path
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail
-from pypsrp.powershell import PowerShell, RunspacePool
-from pypsrp.wsman import WSMan
 
 app = Flask(__name__)
 mail = Mail(app)
@@ -18,25 +18,21 @@ def home():
 def guest():
     if request.method == 'POST':
         user_information = {}
+        if os.path.exists('user_information.json') and os.stat('user_information.json').st_size != 0:
+            with open('user_information.json') as informations_file:
+                user_information = json.load(informations_file)
 
-        user_information[request.form['surname']] = {'surname': request.form['surname']}
-        user_information[request.form['name']] = {'name': request.form['name']}
-        user_information[request.form['mail']] = {'mail': request.form['mail']}
-        user_information[request.form['company']] = {'company': request.form['company']}
-        user_information[request.form['contact_person']] = {'contact_person': request.form['contact_person']}
-        user_information[request.form['number_plate']] = {'number_plate': request.form['number_plate']}
+        guest_user = f'{request.form["surname"]} {request.form["name"]}'
+
+        user_information[guest_user] = {'surname': request.form['surname'],
+                                        'name': request.form['name'],
+                                        'mail': request.form['mail'],
+                                        'company': request.form['company'],
+                                        'contact_person': request.form['contact_person'],
+                                        'number_plate': request.form['number_plate']}
 
         with open('user_information.json', 'w') as information_file:
             json.dump(user_information, information_file)
-
-        with open('password.txt', 'r') as file:
-            admin_password = file.read()
-
-        wsman = WSMan("SRV-DC-VS-01", username="administrator", password=str(admin_password), cert_validation=False)
-
-        with RunspacePool(wsman) as pool:
-            ps = PowerShell(pool)
-
         return render_template('guest.html')
     else:
         return redirect(url_for('home'))
